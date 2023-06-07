ae236c843bf0bf0dbde63766b83d2030a8df695f
Erwin Garcia <erwingarcia4312@gmail.com>
2023-06-07 11:00:28 -0500

    primercommit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+onNewCharacter($event: Event) {
+throw new Error('Method not implemented.');
+}
+
+  public title: string = 'Hola Mundo';
+
+
+}
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,30 @@
+import { Component,  Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+@Input()
+public CharacterList: Character[] = [{
+    name: 'Thunks',
+    power: 10
+}]
+
+//onDeleted  Index : number
+
+@Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+
+  onDeleteCharacter( id?: string ):void {
+
+    if ( !id ) return;
+    this.onDelete.emit( id );
+  }
+
+}
